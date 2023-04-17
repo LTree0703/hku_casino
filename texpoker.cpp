75b2587ca07ee5bf0b9988006c1cacf72e13f95e
LTree0703 <108115115+LTree0703@users.noreply.github.com>
2023-04-17 23:15:56 +0800

    texpoker.cpp card correlation changed, added print table function

--- texpoker.cpp
+++ texpoker.cpp
@@ -12,12 +12,23 @@ using namespace std;
 #define CARD_NUM 52
 #define PLAYER_NUM 5
 
+void read_file(string filename, string &text) // read the file and store it in a string
+{
+    ifstream file(filename);
+    string line;
+    while (getline(file, line))
+    {
+        text += line + "\n";
+    }
+}
+
 struct Card {
     string suit;
     string rank;
 };
 
 struct Player {
+    string name;
     int chips;
     bool hasFolded;
     Card hole_card[2];
@@ -29,14 +40,19 @@ public:
     Card draw_card();
     void print_table();
 
-
 private:
-    Player players[PLAYER_NUM];
+    int phase; // 0 = start; 1 = fold; 2 = turn; 3 = river
+    Card *community_cards;
+    Player *players;
     int cards[CARD_NUM];
 };
 
 Texas::Texas()
 {
+    phase = 0;
+    community_cards = new Card[5];
+    players = new Player[CARD_NUM];
+
     // initialize the card deck: 0 = not drawn; 1 = drawn
     fill(cards, cards+CARD_NUM, 0);
     for (int i = 0; i < PLAYER_NUM; i++)
@@ -45,16 +61,17 @@ Texas::Texas()
         players[i].hasFolded = false;
         players[i].hole_card[0] = draw_card();
         players[i].hole_card[1] = draw_card();
+        community_cards[i] = draw_card();
     }
 }
 
 Card Texas::draw_card()
 {
-    /* define card indices:
+    /* define indices:
         0~12 spade
         13~25 heart
-        26~38 diamond
-        29~51 club*/
+        26~38 club
+        29~51 diamond*/
     Card c;
     int idx = rand() % CARD_NUM;
     while (cards[idx] != 0)
@@ -63,46 +80,66 @@ Card Texas::draw_card()
     }
     cards[idx] = 1;
 
+       
     switch (idx / 13)
     {
         case 0:
-            c.suit = "S";
+            c.suit = "♠"; // spade
             break;
         case 1:
-            c.suit = "H";
+            c.suit = "♥"; // heart
             break;
         case 2:
-            c.suit = "D";
+            c.suit = "♣"; // club
             break;
         case 3:
-            c.suit = "C";
+            c.suit = "♦"; // diamond
             break;
     }
 
     switch (idx % 13)
     {
-        case 0:
-            c.rank = "A";
+        case 9:
+            c.rank = "J"; // jack
             break;
         case 10:
-            c.rank = "J";
+            c.rank = "Q"; // queen
             break;
         case 11:
-            c.rank = "Q";
+            c.rank = "K"; // king
             break;
         case 12:
-            c.rank = "K";
+            c.rank = "A"; // ace
             break;
         default:
-            c.rank = to_string(idx % 13);
+            c.rank = to_string((idx+2) % 13);
             break;
     }
     return c;
 }
 
-void print_table()
+void Texas::print_table()
 {
-    // TODO: access chips and community cards and print on terminal
+    // TODO: access chips, player hole cards and community cards and print on terminal
+    // ♠ ♥ ♣ ♦
+
+    // TODO: render community cards
+
+
+
+    // render hole cards
+    // TODO: what if c.rank == "10"?
+    cout << "You got:" << endl;
+    Card c1 = players[0].hole_card[0];
+    Card c2 = players[1].hole_card[1];
+    printf(".-------.    .-------.\n");
+    printf("|%s      |    |%s      |\n", c1.rank.c_str(), c2.rank.c_str());
+    printf("|       |    |       |\n");
+    printf("|   %s   |    |   %s   |\n", c1.suit.c_str(), c2.suit.c_str());
+    printf("|       |    |       |\n");
+    printf("|      %s|    |      %s|\n", c1.rank.c_str(), c2.rank.c_str());
+    printf("`-------'    `-------'\n");
+
 }
 
 void game_init();
@@ -112,7 +149,6 @@ void clear() // clear console output
 {
     cout << "\x1B[2J\x1B[H";
 }
-
 int main()
 {
     game_init();
@@ -125,19 +161,21 @@ void game_init()
     // set seed value for generating random numbers
     srand((unsigned) time(NULL));
     
+    cout << "♦" << endl;
     cout << R"( _____                       ___      _             )" << endl;
     cout << R"(/__   \_____  ____ _ ___    / _ \___ | | _____ _ __ )" << endl;
     cout << R"(  / /\/ _ \ \/ / _` / __|  / /_)/ _ \| |/ / _ \ '__|)" << endl;
     cout << R"( / / |  __/>  < (_| \__ \ / ___/ (_) |   <  __/ |   )" << endl;
     cout << R"( \/   \___/_/\_\__,_|___/ \/    \___/|_|\_\___|_|   )" << endl;
     cout << "\nWelcome to Texas Poker:)" << endl;
-    cout << "Press enter to continue..." << endl;
+    cout << "Press \033[2menter\033[0m to continue...";
     cin.get();
+    clear();
 
     int round_count = 1;
     while (1)
     {
-        cout << "\033[1;34mGetting ready for round: " << round_count << "\033[0m" << endl;
+        cout << "\n\033[1;34mGetting ready for round: " << round_count << "\033[0m" << endl;
         if (!game_round()) // return true if continue, false otherwise
         {
             break;
@@ -155,11 +193,11 @@ bool game_round()
     int player_turn = 0;
     Texas dealer;
     cout << "Shuffling the cards and dealing..." << endl;
+    sleep(1);
     dealer.draw_card();
 
-
     // TODO: print out the table
-
+    dealer.print_table();
     // TODO: flop: rand three community cards + ask for input
 
     // TODO: turn: rand one community card + ask for input
