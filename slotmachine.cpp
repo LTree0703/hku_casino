5e5426bbff2b2c11d5af890c7b9fb687a62e9284
jaimielee14 <125640497+jaimielee14@users.noreply.github.com>
2023-05-03 15:48:18 +0800

    Create slotmachine.cpp

---
+++ slotmachine.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    srand(time(0)); // seed the random number generator with the current time
+
+    const int NUM_SYMBOLS = 7; // number of symbols in the slot machine
+    const int JACKPOT_ODDS = 100; // odds of hitting the jackpot (1 in JACKPOT_ODDS)
+
+    char symbols[NUM_SYMBOLS] = {'$', '#', '&', '@', '!', '~', '+'}; // array of symbols
+    int credits = 10; // initial number of credits
+    char choice; // player's choice to play again or not
+
+    cout << "Welcome to the HKU CASINO Slot Machine!" << endl;
+
+    do {
+        cout << "You have " << credits << " credits. ";
+        cout << "Press enter to spin the wheel";
+        cin.ignore(); // wait for player to press Enter
+
+        char reel1 = symbols[rand() % NUM_SYMBOLS]; // spin the reels
+        char reel2 = symbols[rand() % NUM_SYMBOLS];
+        char reel3 = symbols[rand() % NUM_SYMBOLS];
+
+        cout << "   " << reel1 << "   " << reel2 << "   " << reel3 << endl;
+
+        if (reel1 == reel2 && reel2 == reel3) { // all three reels match
+            cout << "Jackpot!!! You won 100 credits!" << endl;
+            credits += 100;
+        } else if (reel1 == reel2 || reel1 == reel3 || reel2 == reel3) { // two reels match
+            cout << "Two of a kind! You won 10 credits." << endl;
+            credits += 10;
+        } else { // no reels match
+            cout << "Sorry, you didn't win anything this time." << endl;
+            credits--;
+        }
+
+        if (credits <= 0) { // out of credits
+            cout << "You're out of credits. Game over." << endl;
+            break;
+        }
+
+        // check if player wants to play again
+        cout << "Do you want to play again? (y/n)";
+        cin >> choice;
+        if (choice == 'n') {
+            break;
+        }
+
+    } while (true);
+
+    cout << "Thanks for playing! Goodbye." << endl;
+
+    return 0;
+}
