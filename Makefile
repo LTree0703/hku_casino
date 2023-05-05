40313d0ed75c97c7b7e9e04c5679499a96356ef0
LTree0703 <108115115+LTree0703@users.noreply.github.com>
2023-05-05 23:23:24 +0800

    Renamed file and made small adjustments

--- Makefile
+++ Makefile
@@ -3,11 +3,8 @@ FLAG=-std=c++11
 casino: main.o func.o roulette.o slotmachine.o texpoker.o
 	g++ $(FLAG) $^ -o $@
 
-main.o: main2.cpp roulette.h slotmachine.h texpoker.h
+main.o: main.cpp roulette.h slotmachine.h texpoker.h
 	g++ $(FLAG) -c $< -o $@
-
-bank.o: bank.cpp bank.h
-	g++ $(FLAG) -c $<
 
 func.o: func.cpp func.h
 	g++ $(FLAG) -c $< 
--- main2.cpp
+++ main2.cpp
@@ -1,10 +1,63 @@
-#include <iostream>
+/*#include <iostream>
 #include <ctime>
-#include <unistd.h>
 #include <fstream>
 #include <string>
 
-#include "func.h"
+#include "menu.h"
+#include "bank.h"
+#include "roulette.h"
+#include "texpoker.h"
+#include "slotmachine.h"
+
+using namespace std;
+
+int main() {
+    double balance = 1000.0;
+    char input;
+
+    cout << "Welcome to the Casino Game!" << endl;
+
+    while (!login()) {
+        cout << "Invalid account name or password. Please try again." << endl;
+    }
+
+    cout << "Login successful. Enjoy the game!" << endl;
+
+    while (true) {
+        cout << "Press ESC to open the menu." << endl;
+        input = _getch();
+
+        if (input == 27) { // ASCII code for ESC key
+            displayMenu();
+            input = _getch();
+
+            switch (input) {
+                case '1':
+                    restartGame();
+                    break;
+                case '2':
+                    bankMenu();
+                    input = _getch();
+                    if (input == '1') {
+                        deposit(balance);
+                    } else if (input == '2') {
+                        withdraw(balance);
+                    }
+                    break;
+                case '3':
+                    exitGame();
+                    return 0;
+                default:
+                    cout << "Invalid input. Try again." << endl;
+            }
+        }
+    }
+    return 0;
+}*/
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <string>
 #include "menu.h"
 #include "bank.h"
 #include "roulette.h"
@@ -14,187 +67,89 @@
 using namespace std;
 
 
-void game_init();
-void game_menu();
-void setup_player();
-void alter_value(string name, int chips);
-
-int main() 
-{
-    clear();
-    cout << "__   __  ___   _  __   __    _______  _______  _______  ___   __    _  ________ " << endl;
-    cout << "|  | |  ||   | | ||  | |  |  |       ||   _   ||       ||   | |  |  | ||       |" << endl;
-    cout << "|  |_|  ||   |_| ||  | |  |  |       ||  |_|  ||  _____||   | |   |_| ||   _   |" << endl;
-    cout << "|       ||      _||  |_|  |  |       ||       || |_____ |   | |       ||  | |  |" << endl;
-    cout << "|       ||     |_ |       |  |      _||       ||_____  ||   | |  _    ||  |_|  |" << endl;
-    cout << "|   _   ||    _  ||       |  |     |_ |   _   | _____| ||   | | | |   ||       |" << endl;
-    cout << "|__| |__||___| |_||_______|  |_______||__| |__||_______||___| |_|  |__||_______|" << endl;
-    cout << "Loading game..." << endl;
-    sleep(1);
-    game_init();
-    return 0;
-}
-
-void game_init()
-{
-    clear();
-    // check if user data exists
-    bool newGame = false;
-    ifstream user("data.txt");
-    if (!user.good())
-    {
-        newGame = true;
-    }
-
+void restartgame();
+int main() {
+    double balance = 1000.0;
+    char input;
     ifstream fin;
-    fin.open("menu.txt");
-    string menu, line;
-    int line_count = 0;
-    if (fin.fail())
-    {
-        cout << "An error occurred, please try again." << endl;
-        return;
+    
+    // login and validateCredentials to compute
+    while (!login()) {
+        cout << "Invalid account name or password. Please try again." << endl;
     }
 
-    while (getline(fin, line))
-    {
-        line_count++;
-        if (line_count == 8 && !newGame)
-            menu += "|                  New Profile  (3)                  |\n";
-        else if (line_count == 10)
-            menu += line;
-        else
-            menu += line + "\n";
-    }
-    fin.close();
+    cout << "Login successful. Enjoy the game!" << endl;
 
-    string input;
-    while (true)
-    {
-        clear();
-        cout << menu << endl;
-        cout << "Please choose an option... ";
-        getline(cin, input);
-        if (input == "1")
-        {
-            if (newGame)
-                setup_player();
-            break;
-        }
-        else if (input == "2")
-        {
-            cout << "Thanks for playing!" << endl;
-            exit(0);
-        }
-        else if (input == "3")
-        {
-            setup_player();
-            break;
-        }
-        cout << "Invalid output, please try again.";
-        cin.get();
-    }
-    game_menu();
-}
-
-void game_menu()
-{
-    // load player data
-    static string player_name;
-    static int chips;
-    ifstream fin;
-
-    // load into game menu
-    while (true)
-    {
-        clear();
-        // fetch user data
-        fin.open("data.txt");
-        if (fin.fail())
-        {
-            cout << "Seems like player data is missing/crashed, please set up a new user profile:(" << endl;
-            cout << "Exiting Game..." << endl;
-            return;
-        }
-        getline(fin, player_name);
-        fin >> chips;
-        fin.close();
-
-        // print menu on console
-        fin.open("game.txt");
-        if (fin.fail())
-        {
-            cout << "An error occurred, please try again." << endl;
-            return;
-        }
+    while (true) {
+        fin.open("menu.txt");
+    if (fin.fail()){
+        cout << "Error !" << endl;
+    }else{
         string line;
-        while (getline(fin,line))
-        {
+        while (getline(fin,line)){
             cout << line << endl;
         }
         fin.close();
-        cout << "Hello, " << player_name << ". You currently have " << chips << " chips." << endl;
-        if (chips <= 0)
-        {
-            cout << "You have no chips now. You have to create a new user profile again.";
-            cin.get();
-            remove("data.txt"); // delete original file
-            game_init();
-            return;
+    
+        cin >> input;
+
+        switch (input) {
+            case '1':
+                restartGame();
+                    
+            case '2':
+                bankMenu();
+                cin >> input;
+                if (input == '1') {
+                    deposit(balance);
+                } else if (input == '2') {
+                    withdraw(balance);
+                }
+                
+            case 27:
+                exitGame();
+                return 0;
+                default:
+                    cout << "Invalid input. Try again." << endl;
+            }
         }
-        cout << "Which game do you want to play? ";
-        // wait for choice 
-        string choice;
-        getline(cin, choice);
-        if (choice == "1")
-        {
-            roulette_init(player_name, chips);
-        }
-        else if (choice == "2")
-        {
-            slot_init(player_name, chips);
-        }
-        else if (choice == "3")
-        {
-            texpoker_init(player_name, chips);
-        }
-        else if (choice == "4")
-        {
-            game_init();
-            return;
-        }
-        else
-        {
-            cout << "Invalid input, please try again." << endl;
-            cin.get();
-        }
-        alter_value(player_name, chips);
     }
+
+
+
+
 }
 
-void setup_player()
-{
-    string name;
-    int init_chips = 100;
+void restartGame() {
+    // Code to restart the game
+    ifstream fin;
+    while (true){
+    cout << "Restarting the game..." << endl;
+    fin.open("game.txt");
+    if (fin.fail()){
+        cout << "Error !" << endl;
+    }else{
+        string line2;
+        while (getline(fin,line2)){
+            cout << line << endl;
+        }
+        fin.close();
 
-    cout << "Please enter your name: ";
-    getline(cin, name);
-    cout << "Welcome " << name << "!" << endl;
-    cout << "You are given " << init_chips << " chips as your initial fund. Enjoy the game!" << endl;
-    cout << "Press enter to continue...";
-    cin.get();
 
-    // store data into file
-    ofstream fout("data.txt");
-    fout << name << endl;
-    fout << init_chips;
-    fout.close();
-}
+    int choice;
+    cin >> choice;
+    if (choice == 1){
+        roulette_init();}
+    else if (choice == 2){
+        slot_init();
+    }    
+    else if (choice == 3){
+        texpoker_init();
+    }
+    else{
+        break;
+    }
+    
+}}}
 
-void alter_value(string player_name, int chips)
-{
-    ofstream fout("data.txt");
-    fout << player_name << endl;
-    fout << chips;
-    fout.close();
-}
+
